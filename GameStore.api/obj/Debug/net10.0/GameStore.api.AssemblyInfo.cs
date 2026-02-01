560a4ba893ddad2132ea693a1d2423cebc5452aa
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2026-02-01 10:25:04 +0800

    Initialize ASP.NET Core project

--- GameStore.api/obj/Debug/net10.0/GameStore.api.AssemblyInfo.cs
+++ GameStore.api/obj/Debug/net10.0/GameStore.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23d5e9c693c5343c19021e1e6a0bd93506fe5cd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,10 @@
+
+- dotnet list
+- Control + Shift + P (.NET)
+
+- dotnet build
+- Control + Shift + B
+
+- F5 / Control + F5
+
+- dotnet watch --project Todo.Api/Todo.Api.csproj run
